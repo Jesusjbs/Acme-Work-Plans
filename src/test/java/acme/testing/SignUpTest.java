1fa1b72421d6bbfd640d22c8fc19fbe5856717bc
Jesus <jesbarsig@alum.us.es>
2021-03-26 19:28:50 +0100

    Acme Planner's customization

--- src/test/java/acme/testing/SignUpTest.java
+++ src/test/java/acme/testing/SignUpTest.java
@@ -31,7 +31,7 @@ public class SignUpTest extends AbstractTest {
 	public void beforeAll() {
 		super.beforeAll();
 
-		super.setBaseCamp("http", "localhost", "8080", "/Starter-Project", "/master/welcome", "?language=en&debug=true");
+		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
 		super.setAutoPausing(true);
 
 		this.signIn("administrator", "administrator");

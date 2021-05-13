92f27916ace24e03c5e8a686edbc5c0fad34c779
Fernando <ferherque@alum.us.es>
2021-05-13 13:36:04 +0200

    Test para listar shouts anónimos

---
+++ src/test/java/acme/testing/AcmePlannerTest.java
@@ -0,0 +1,70 @@
+package acme.testing;
+
+import org.hibernate.internal.util.StringHelper;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class AcmePlannerTest extends AcmeTest {
+	
+	// Internal state ---------------------------------------------------------
+
+	// Lifecycle management ---------------------------------------------------
+
+	@Override
+	@BeforeAll
+	public void beforeAll() {
+		super.beforeAll();
+
+		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
+		super.setAutoPausing(false);
+		
+		this.navigateHome();
+		this.signIn("administrator", "administrator");
+		super.clickOnMenu("Administrator", "Populate DB (samples)");
+		super.checkAlertExists(true);		
+		this.signOut();
+	}
+
+	// Business methods -------------------------------------------------------
+	
+	protected void signIn(final String username, final String password) {
+		assert !StringHelper.isBlank(username);
+		assert !StringHelper.isBlank(password);
+		
+		super.navigateHome();
+		super.clickOnMenu("Sign in", null);
+		super.fillInputBoxIn("username", username);
+		super.fillInputBoxIn("password", password);
+		super.fillInputBoxIn("remember", "true");
+		super.clickOnSubmitButton("Sign in");
+		super.checkSimplePath("/master/welcome");
+		super.checkLinkExists("Account");
+	}
+
+	protected void signOut() {
+		super.navigateHome();
+		super.clickOnMenu("Sign out", null);
+		super.checkSimplePath("/master/welcome");
+	}
+
+	protected void signUp(final String username, final String password, final String name, final String surname, final String email, final String phone) {
+		assert !StringHelper.isBlank(username);
+		assert !StringHelper.isBlank(password);
+		assert !StringHelper.isBlank(name);
+		assert !StringHelper.isBlank(surname);
+		assert !StringHelper.isBlank(email);
+		// phone is nullable
+
+		super.navigateHome();
+		super.clickOnMenu("Sign up", null);	
+		super.fillInputBoxIn("username", username);
+		super.fillInputBoxIn("password", password);
+		super.fillInputBoxIn("confirmation", password);
+		super.fillInputBoxIn("identity.name", name);
+		super.fillInputBoxIn("identity.surname", surname);
+		super.fillInputBoxIn("identity.email", email);
+		super.fillInputBoxIn("identity.phone", phone);
+		super.fillInputBoxIn("accept", "true");
+		super.clickOnSubmitButton("Sign up");
+		super.checkSimplePath("/master/welcome");
+	}
+}
---
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutListTest.java
@@ -0,0 +1,29 @@
+package acme.testing.anonymous.shout;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AnonymousShoutListTest extends AcmePlannerTest {
+	
+	// Lifecycle management ---------------------------------------------------
+	
+	// Test cases -------------------------------------------------------------
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/shout/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)	
+	public void listPositive(final int recordIndex, final String moment, final String author, final String text) {
+
+		super.clickOnMenu("Anonymous", "List recent shouts");
+		
+		super.checkColumnHasValue(recordIndex, 0, moment);
+		super.checkColumnHasValue(recordIndex, 1, author);
+		super.checkColumnHasValue(recordIndex, 2, text);
+	}
+	
+	// Ancillary methods ------------------------------------------------------
+
+}
